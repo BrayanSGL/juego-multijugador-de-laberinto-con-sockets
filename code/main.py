e05111aca63770dc6b16bd4e995cc8c039d9d416
Brayan Galeano <bsgaleanol@correo.udistrital.edu.co>
2022-11-23 22:14:04 -0600

    Reinicio

--- code/main.py
+++ code/main.py
@@ -1,6 +1,4 @@
 # Laberinto + sockets + threads + pygame + python 3.10
-# Autors: Brayan Galeano Lara y Paula Anagones Murcia
-# Date: November 2022
 
 import pygame
 import sys
@@ -9,27 +7,7 @@ import level
 
 
 class Game:
-    def __init__(self):
-        pygame.init()
-        self.screen = pygame.display.set_mode(
-            (settings.WIDTH, settings.HEIGHT))
-        pygame.display.set_caption(settings.TITLE)
-        self.clock = pygame.time.Clock()
-        self.running = True
-        self.level = level.Level()
-
-    def run(self):
-        while self.running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
-                    sys.exit()
-
-            self.screen.fill(settings.BLACK)
-            pygame.display.update()
-            # LEVEL
-            self.clock.tick(settings.FPS)
-
+    pass
 
 if __name__ == "__main__":
     game = Game()
