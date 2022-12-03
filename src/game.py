167595a8c4b650abd9919c901192c6d734411f72
Brayan Galeano <bsgaleanol@correo.udistrital.edu.co>
2022-12-03 16:52:05 -0600

    Ya se muestra el jugador

--- src/game.py
+++ src/game.py
@@ -10,7 +10,17 @@ class Player():
     def __init__(self, start_pos):
         self.x = start_pos[0]
         self.y = start_pos[1]
+        self.image ={
+            'up': pygame.transform.scale(pygame.image.load('assets/player/back.png'), (TILE_SIZE,TILE_SIZE)),
+            'down': pygame.transform.scale(pygame.image.load('assets/player/front.png'), (TILE_SIZE,TILE_SIZE)),
+            'left': pygame.transform.scale(pygame.image.load('assets/player/left.png'), (TILE_SIZE,TILE_SIZE)),
+            'right': pygame.transform.scale(pygame.image.load('assets/player/right.png'), (TILE_SIZE,TILE_SIZE))
+        }
         print(self.x, self.y)
+
+    def draw(self, screen, direction):
+        screen.blit(self.image[direction], (self.x, self.y))
+
 
 
 class Game:
@@ -24,6 +34,8 @@ class Game:
 
     def run(self):
         print(self.width, self.height)
+        direction = ['up', 'down', 'left', 'right']
+        direction_str = 'down'
         clock = pygame.time.Clock()
         is_running = True
         while is_running:
@@ -35,18 +47,31 @@ class Game:
                     is_running = False
 
             keys = pygame.key.get_pressed()
-
             # Player movement with WASD or arrow keys
+            if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+                direction_str = direction[2]
+                self.player.x -= 1
+            if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+                direction_str = direction[3]
+                self.player.x += 1
+            if keys[pygame.K_w] or keys[pygame.K_UP]:
+                direction_str = direction[0]
+                self.player.y -= 1
+            if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+                direction_str = direction[1]
+                self.player.y += 1
+
 
             # Update canvas
             self.canvas.draw_background()
-            self.player.draw(self.canvas.get_canvas())
+            self.player.draw(self.canvas.get_canvas(),direction_str)
             self.canvas.update()
 
         pygame.quit()
 
     def send_data(self):
         pass
+
 
 class Canvas:
     def __init__(self, width, height, title):
@@ -56,7 +81,7 @@ class Canvas:
         self.screen = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption(self.title)
 
-    @staticmethod 
+    @staticmethod
     def update():
         pygame.display.update()
 
@@ -72,7 +97,7 @@ class Canvas:
 
     def draw_background(self):
         self.screen.fill((0, 0, 0))
-        for row in range(len(WORLD_MAP)):
-            for col in range(len(WORLD_MAP[row])):
-                tile = WORLD_MAP[row][col]
-                self.screen.blit(tile, (col * TILE_SIZE, row * TILE_SIZE))
+        # for row in range(len(WORLD_MAP)):
+        #     for col in range(len(WORLD_MAP[row])):
+        #         tile = WORLD_MAP[row][col]
+        #         self.screen.blit(tile, (col * TILE_SIZE, row * TILE_SIZE))
